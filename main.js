64e15519d7d4e75470321ffc185db067470169d4
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2021-12-06 21:46:12 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+var billamount=document.querySelector("#bill-amount");
+var cashamount=document.querySelector("#cash-amount");
+var btncheck=document.querySelector("#check-button");
+
+btncheck.addEventListener("click",() => console.log("clicked"));
